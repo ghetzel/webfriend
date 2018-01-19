291050c12fe5946ff1cb146c492acbb0e1a9640b
Gary Hetzel <garyhetzel@gmail.com>
2018-01-19 11:03:38 -0500

    Update screenshot.fs

--- examples/screenshot.fs
+++ examples/screenshot.fs
@@ -1,11 +1,7 @@
 #!/usr/bin/env webfriend
 
 # navigate to Hacker News
-go "https://news.ycombinator.com" -> $www
-
-# log the result of loading the page
-log "Page loaded in {www[timing][requestTime]}ns"
-log "URL {www[url]}: Loaded with HTTP {www[status]}"
+go "https://news.ycombinator.com"
 
 # take a screenshot of the current page
 page::screenshot "hackernews.png"
